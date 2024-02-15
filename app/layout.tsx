12a067c449d6d2debad36a80aa284e968a1f3b1f
Vlad <vla.koncha@gmail.com>
2024-02-15 14:58:52 +0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['cyrillic'],
+  weight: ['400', '500', '700'],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | ТревелБук',
+    default: 'ТревелБук',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
